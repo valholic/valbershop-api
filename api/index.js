7e54b4543b029febdfe80d5c9259080368f39f1b
valholic <yansenlodova@gmail.com>
2025-09-20 21:26:03 +0800

    cors 11

--- api/index.js
+++ api/index.js
@@ -1,49 +1,15 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const connectDB = require('../src/config/dbConfig');
-const createAdminAccount = require('../src/script/admin');
-const handleError = require('../src/middlewares/handleError');
+import app from "../index";
+import serverless from "serverless-http";
+import connectDB from "../src/config/dbConfig";
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+let isConnected = false;
 
-// parser & cors set-up
-app.use(cors({
-    origin: "https://valholic.github.io",
-    methods: ["POST", "GET", "DELETE", "PUT", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
-app.use(express.json());
+const handler = async (req, res) => {
+    if (!isConnected) {
+        await connectDB();
+        isConnected = true;
+    }
+    return app(req, res);
+};
 
-// Routes field
-const signupRoute = require('../src/routes/signup');
-const loginRoute = require('../src/routes/login');
-const userRoute = require('../src/routes/user');
-const galleryRoute = require('../src/routes/gallery');
-const shopRoute = require('../src/routes/shop');
-const testimonyRoute = require('../src/routes/testimony');
-
-app.use('/v1/user', signupRoute);
-app.use('/v1/auth', loginRoute);
-app.use('/v1/api', userRoute);
-app.use('/v1/gallery', galleryRoute);
-app.use('/v1/shop', shopRoute);
-app.use('/v1/testimony', testimonyRoute);
-
-connectDB().then(() => {
-    // create admin account
-    createAdminAccount();
-})
-
-// Error handle
-app.use(handleError);
-
-module.exports = app;
-// connectDB().then(() => {
-//     app.listen(PORT, () => {
-//         console.log(`Server is running at http://localhost:${PORT}`);
-//     });
-// });
+export default serverless(handler);
---
+++ index.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const connectDB = require('./src/config/dbConfig');
+const createAdminAccount = require('./src/script/admin');
+const handleError = require('./src/middlewares/handleError');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// parser & cors set-up
+app.use(express.json());
+app.use(cors({
+    origin: "https://valholic.github.io",
+    methods: ["POST", "GET", "DELETE", "PUT", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
+app.options("*", cors());
+
+// Routes field
+const signupRoute = require('./src/routes/signup');
+const loginRoute = require('./src/routes/login');
+const userRoute = require('./src/routes/user');
+const galleryRoute = require('./src/routes/gallery');
+const shopRoute = require('./src/routes/shop');
+const testimonyRoute = require('./src/routes/testimony');
+
+app.use('/v1/user', signupRoute);
+app.use('/v1/auth', loginRoute);
+app.use('/v1/api', userRoute);
+app.use('/v1/gallery', galleryRoute);
+app.use('/v1/shop', shopRoute);
+app.use('/v1/testimony', testimonyRoute);
+
+connectDB().then(() => {
+    // create admin account
+    createAdminAccount();
+})
+
+// Error handle
+app.use(handleError);
+
+module.exports = app;
+// connectDB().then(() => {
+//     app.listen(PORT, () => {
+//         console.log(`Server is running at http://localhost:${PORT}`);
+//     });
+// });
