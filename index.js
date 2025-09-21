0aced4a34f2fbdcba3fc11567923adbbf7b1881d
valholic <yansenlodova@gmail.com>
2025-09-22 07:59:42 +0800

    cors keknya last

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const connectDB = require('./src/config/dbConfig');
 const createAdminAccount = require('./src/script/admin');
 const handleError = require('./src/middlewares/handleError');
@@ -11,13 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 // parser & cors set-up
 app.use(express.json());
-app.use(cors({
-    origin: "https://valholic.github.io",
-    methods: ["POST", "GET", "DELETE", "PUT", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
-app.options("*", cors());
+app.use(cors());
 
 // Routes field
 const signupRoute = require('./src/routes/signup');
@@ -39,9 +32,8 @@ createAdminAccount();
 // Error handle
 app.use(handleError);
 
-module.exports = app;
-// connectDB().then(() => {
-//     app.listen(PORT, () => {
-//         console.log(`Server is running at http://localhost:${PORT}`);
-//     });
-// });
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running at http://localhost:${PORT}`);
+    });
+});
